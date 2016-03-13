006fae3818e259428d8584463cd76364cb079ec5
CHEN <470752945@qq.com>
2016-03-13 17:43:43 +0800

    DRY

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -28,6 +33,7 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
         #页面中还有一个文本框，可以输入其他但待办事项
@@ -38,14 +44,17 @@ class NewVisitorTest(unittest.TestCase):
 
         #页面再次更新清单中显示了这两个待办事项
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
         #        any(row.text == '1: Buy peacock feathers' for row in rows),
         #        "New to-do item did not appear in table -- its text was:\n%s" % (table.text,)
         #        )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock to make a fly', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('2: Use peacock to make a fly', [row.text for row in rows])
+        
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock to make a fly')
 
         self.fail('Finish the test!')
 
