380ae8e274ab2ea42b9b9638907d5eaf86acf366
CHEN <470752945@qq.com>
2016-03-13 17:04:13 +0800

    functional_tests.py|home.html|tests.py|views.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -17,7 +18,8 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_test = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_test)
-
+        
+        #输入
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
@@ -25,16 +27,26 @@ class NewVisitorTest(unittest.TestCase):
                 )
 
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
+
+
+        #页面中还有一个文本框，可以输入其他但待办事项
+        #输入"Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        #页面再次更新清单中显示了这两个待办事项
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-                )
-        
+        #self.assertTrue(
+        #        any(row.text == '1: Buy peacock feathers' for row in rows),
+        #        "New to-do item did not appear in table -- its text was:\n%s" % (table.text,)
+        #        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock to make a fly', [row.text for row in rows])
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
